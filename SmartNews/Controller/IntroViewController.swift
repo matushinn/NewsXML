092c5c02f4bec9d7f3f067115dfddce6f9ce0f5e
shotaro-ooe <shoooe97@gmail.com>
2021-07-21 00:13:43 +0900

    Lottie導入成功

--- SmartNews/Controller/IntroViewController.swift
+++ SmartNews/Controller/IntroViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Lottie
 
-class IntroViewController: UIViewController {
+class IntroViewController: UIViewController ,UIScrollViewDelegate{
 
     @IBOutlet weak var scrollView: UIScrollView!
     
@@ -22,7 +22,23 @@ class IntroViewController: UIViewController {
         
         //ページングができるようになる。
         scrollView.isPagingEnabled = true
-        // Do any additional setup after loading the view.
+
+        setUpScroll()
+        
+        //最初は0が入るので一番左から、次は1ページ次からのようになっていく。
+        for i in 0...4{
+            let animationView = AnimationView()
+            let animation = Animation.named(onboardArray[i])
+            animationView.frame = CGRect(x: CGFloat(i)*view.frame.size.width, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+            
+            animationView.animation = animation
+            animationView.contentMode = .scaleAspectFit
+            animationView.loopMode = .loop
+            animationView.play()
+            scrollView.addSubview(animationView)
+            
+            
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,6 +49,20 @@ class IntroViewController: UIViewController {
     }
     
     func setUpScroll(){
+        scrollView.delegate = self
+        
+        scrollView.contentSize = CGSize(width: view.frame.size.width*5, height: scrollView.frame.size.height)
+        
+        for i in 0...4{
+            //最初は0が入るので一番左から、次は1ページ次からのようになっていく。
+            let onboardLabel = UILabel(frame: CGRect(x: CGFloat(i)*view.frame.size.width, y: view.frame.height/3, width: scrollView.frame.size.width, height: scrollView.frame.size.height))
+            
+            onboardLabel.font = UIFont.boldSystemFont(ofSize: 15.0)
+            onboardLabel.textAlignment = .center
+            onboardLabel.text = onboardStringArray[i]
+            scrollView.addSubview(onboardLabel)
+            
+        }
         
     }
 
